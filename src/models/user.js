88ebc39e386b034c5f567a229f1cdbede3c725e4
dharmil-patel <dharmilpatel06@gmail.com>
2023-10-18 23:00:34 +0530

    perform database connection & user schema changes

--- src/models/user.js
+++ src/models/user.js
@@ -1,15 +1,13 @@
 import mongoose from 'mongoose'
 
-const userSchema = new mongoose.Schema({
-    firstName: String,
-    lastName: String,
-    mobile: String,
-    isMobileVerified: Boolean,
-    isActive: Boolean,
-    createdAt: Date,
-    updatedAt: Date,
-  })
+const UserSchema = new mongoose.Schema({
+    firstName: { type: String, required: false, trim: true },
+    lastName: { type: String, required: false, trim: true },
+    mobile: { type: String, required: true, unique: true, trim: true },
+    isMobileVerified: { type: Boolean, default: false, required: false },
+    isActive: { type: Boolean, default: false, required: false },
+  }, { timestamps: true })
 
-  const User = mongoose.model('users', userSchema);
+  const User = mongoose.model('users', UserSchema);
 
   export default User;

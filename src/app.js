47f6457ad50b7102816c8bdf838429ba5d825e53
Zuber Shekh <zuber.shekh7@gmail.com>
2023-10-27 23:26:08 +0530

    setup cors support

--- src/app.js
+++ src/app.js
@@ -1,10 +1,22 @@
-import express from 'express';
-import customersRoutes from './routers/customers.js';
+import cors from "cors";
+import dotenv from "dotenv";
+import express from "express";
 
+import customersRoutes from "./routers/customers.js";
+
+dotenv.config();
 const app = express();
 
+const ALLOWED_ORIGIN = process.env.ALLOWED_ORIGIN;
+
+const corsOptions = {
+  origin: ALLOWED_ORIGIN,
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
-app.use('/customers', customersRoutes);
+app.use("/customers", customersRoutes);
 
 export default app;
